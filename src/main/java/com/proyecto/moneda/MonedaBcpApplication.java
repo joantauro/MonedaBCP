172f27582e54b9a56cc3a0e5d29bbc116f788a66
joelromerog.11@gmail.com <joelromerog.11@gmail.com>
2022-03-10 10:36:55 -0500

    Inicio de Proyecto

---
+++ src/main/java/com/proyecto/moneda/MonedaBcpApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.moneda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonedaBcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonedaBcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/moneda/MonedaBcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.moneda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonedaBcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
